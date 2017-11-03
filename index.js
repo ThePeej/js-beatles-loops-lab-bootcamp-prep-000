9da18d2041b7ecddb73cf2ba6986dbfb320bae18
PJ Frias <peejfrias@gmail.com>
2017-11-03 21:23:28 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
-
+function theBeatlesPlay(musicians, instruments) {
+  var plays = {}
+  for (let n = 0; n < musicians.length;n++) {
+    plays.push(musicians[n] + " plays " + instruments[n])
+  }
+  return plays
+}
 
 function johnLennonFacts(array) {
   var n = 0
   while (n < array.length) {
-    array[n] = array[n]+"!!!"
+    array[n] = array[n] + "!!!"
     n++
   }
   return array
