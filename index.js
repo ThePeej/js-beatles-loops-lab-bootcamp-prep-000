cb909be4019106f792bf437c86423de088156c72
PJ Frias <peejfrias@gmail.com>
2017-11-03 21:14:18 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,9 +2,8 @@
 
 function johnLennonFacts(array) {
   var n = 0
-  var exclam = "!!!"
   while (n < array.length) {
-    array = array.splice(n,1,array[n]+exclam)
+    array[n] = array[n]+"!!!"
     n++
   }
   return array
